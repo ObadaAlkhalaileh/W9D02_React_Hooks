a08c759345f4f3f7298581e7acc3e877d2db88c4
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-05-25 02:04:55 +0400

    pulse check 9

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 // jsx
@@ -11,20 +12,37 @@ export default function App() {
   const [title, setTitle] = useState()
   const [body, setBody] = useState()
 
+  useEffect(() => {
+    console.log('placholder posts');
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/users`)
+      .then((response) => {  
+        setPosts(response.data);
+      })
+      .catch((err) => {
+        console.log('ERR: ', err);
+      });
+  };
+
 const addPost=()=>{
   const newPost=[{ userId:userId, id:id, title:title, body:body }]
 setPosts([...posts,...newPost])
-  console.log(posts)
+//we cant use push here because posts isnt a normal array 
+//and push isnt a prototype for it
+  // console.log(posts)
 };
 
   return(<> 
   {/* {console.log(posts)} */}
-  <div><h1>Blog App</h1></div>;
+  <div><h1>Blog App</h1></div>
 {posts.map((elem,i)=>{
 return<div key={i}> <h3>{elem.title} </h3>
 <p>{elem.body}</p>
 </div>
-
 })} 
 <input type="text" placeholder="user id" onChange={(e) => {
           setUserId(e.target.value);
