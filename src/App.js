2265ff76b5cba0934b1adbdc95f6836443aaf087
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-05-25 00:58:45 +0400

    pulse check 6

--- src/App.js
+++ src/App.js
@@ -6,12 +6,23 @@ export default function App() {
   const [posts, setPosts] = useState([{ userId: 2, id: 102, title: "Animals", body: "Animals are nice" },
   { userId: 7, id: 107, title: "Programming", body: "focus to be a reliable programmer" }]);
 
+  const [userId, setUserId] = useState()
+  const [id, setId] = useState()
+  const [title, setTitle] = useState()
+  const [body, setBody] = useState()
+
   return(<> 
   {console.log(posts)}
   <div><h1>Blog App</h1></div>;
 {posts.map((elem,i)=>{
-return<div> <h3 key={i}>{elem.title} </h3>
-<p>{elem.body}</p> </div>
+return<div key={i}> <h3>{elem.title} </h3>
+<p>{elem.body}</p>
+<button>click here!</button>
+<input type="text" placeholder="type here"/>
+<input type="text" placeholder="type here"/>
+<input type="text" placeholder="type here"/>
+<input type="text" placeholder="type here"/>
+ </div>
 })
 } 
   </>)};
