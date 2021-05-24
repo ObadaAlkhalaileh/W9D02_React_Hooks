553a42af7a5e231ac0dcfc7b393cd54eabde4e2c
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-05-25 00:45:38 +0400

    pulse check 4,5

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, { useState } from 'react';
 
 
 // jsx
-export default function App () {
-  const [posts, setPosts] = useState([{userId:2,id:102,title:"Animals",body:"Animals are nice"},
-  {userId:7,id:107,title:"Programming",body:"focus to be a reliable programmer"}]);
+export default function App() {
+  const [posts, setPosts] = useState([{ userId: 2, id: 102, title: "Animals", body: "Animals are nice" },
+  { userId: 7, id: 107, title: "Programming", body: "focus to be a reliable programmer" }]);
 
-  return <div><h1>Blog App</h1></div>;
-};
-
+  return(<> 
+  {console.log(posts)}
+  <div><h1>Blog App</h1></div>;
+{posts.map((elem,i)=>{
+return<div> <h3 key={i}>{elem.title} </h3>
+<p>{elem.body}</p> </div>
+})
+} 
+  </>)};
