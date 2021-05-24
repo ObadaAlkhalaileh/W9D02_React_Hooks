af155efeb748020ded88b04b4fdfefd2f286c80e
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-05-25 00:16:52 +0400

    pulse check 1,2,3,4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 // jsx
-const App = () => {
-  return <h1>MERAKI Academy</h1>;
+export default function App () {
+  const [posts, setPosts] = useState([{userId:2,id:102,title:"Animals",body:"Animals are nice"},
+  {userId:7,id:107,title:"Programming",body:"focus to be a reliable programmer"}]);
+
+  return <div><h1>Blog App</h1></div>;
 };
 
-export default App
